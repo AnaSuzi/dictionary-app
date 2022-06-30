c6540908287e4c398964136ea3a987f2f3f7c0d0
AnaSuzi <104015770+AnaSuzi@users.noreply.github.com>
2022-06-30 16:42:55 +0200

    set up a search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"> </header>
+      <header className="App-header">
+        dictionary
+        <div className="Undertitle">look for your word ...</div>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
     </div>
   );
 }
